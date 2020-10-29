493620e5a71c4f6d75c606adf59a819839566316
Matt Hackmann <matt@dxprog.com>
2020-10-28 20:32:33 -0700

    observation types as an enum

--- src/interfaces/observation.ts
+++ src/interfaces/observation.ts
@@ -1,7 +1,17 @@
+export enum ObservationType {
+  TEMPERATURE = 'temperature',
+  HUMIDITY = 'humidity',
+  BAROMETRIC_PRESSURE = 'barometric_pressure',
+  AIR_QUALITY_PM25 = 'air_quality_pm25',
+  AIR_QUALITY_PM10 = 'air_quality_pm10',
+  WIND_SPEED = 'wind_speed',
+  WIND_RIECTION = 'wind_direction',
+}
+
 export interface IObservation {
   stationKey: string;
   stationName: string;
   observationTime: number;
-  observationType: string;
+  observationType: ObservationType;
   observationValue: number;
 }
